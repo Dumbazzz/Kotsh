a2c042c6705ea7a05a14129cb96da43d0f018868
Biometric <51202001+biom3tric@users.noreply.github.com>
2019-07-03 02:05:06 +0800

    Block: Possibility to make block following
    * Much more stable

--- Blocks/Block.cs
+++ Blocks/Block.cs
@@ -1,6 +1,9 @@
 ﻿using Kotsh.Blocks.Action;
 using Kotsh.Blocks.Util;
 using Kotsh.Objects;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Kotsh.Blocks
 {
@@ -27,6 +30,22 @@ namespace Kotsh.Blocks
         public Source Source;
 
         /// <summary>
+        /// Combo
+        /// </summary>
+        public string combo;
+
+        /// <summary>
+        /// Username and Password
+        /// </summary>
+        public string username;
+        public string password;
+
+        /// <summary>
+        /// Can continue automation
+        /// </summary>
+        public bool running = true;
+
+        /// <summary>
         /// Store core instance and initialize blocks
         /// </summary>
         /// <param name="core">Kotsh instance</param>
@@ -41,5 +60,51 @@ namespace Kotsh.Blocks
             this.StringUtil = new StringUtil(this);
             this.Source = new Source(this);
         }
+
+        /// <summary>
+        /// Execute a list of actions
+        /// </summary>
+        /// <param name="combo">Save combo for automatic usage</param>
+        /// <param name="methods">List of actions</param>
+        public void Automate(string combo, List<System.Action> methods)
+        {
+            // Reset running
+            running = true;
+
+            // Save combo
+            if (combo != "")
+                SetCombo(combo);
+
+            // Execute every methods
+            foreach (System.Action method in methods)
+            {
+                // Check if running is allowed
+                if (running)
+                {
+                    // Invoke method
+                    method.Invoke();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set a combo and split it
+        /// </summary>
+        /// <param name="combo">Combo</param>
+        private void SetCombo(string combo)
+        {
+            // Save full definition
+            this.combo = combo;
+
+            // Split and save definitions
+            username = combo.Split(':')[0];
+            password = combo.Split(':')[1];
+        }
+
+        /// <summary>
+        /// Stop block running
+        /// </summary>
+        public void Stop()
+            => running = false;
     }
 }
